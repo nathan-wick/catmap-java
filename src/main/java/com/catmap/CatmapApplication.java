e2746501c627a115e0ac8ddd7b7ae88aaff4f5f5
Nathan Wick <nathanwick2001@gmail.com>
2023-02-17 13:36:45 -0500

    Initial

---
+++ src/main/java/com/catmap/CatmapApplication.java
@@ -0,0 +1,13 @@
+package com.catmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catmap/catmap/CatmapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catmap.catmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatmapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
