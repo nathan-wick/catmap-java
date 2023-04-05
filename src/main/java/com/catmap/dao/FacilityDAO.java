506c806bb69ed84215ccd49c5e969129baa99d78
Pratik <101081557+pratxks@users.noreply.github.com>
2023-04-05 06:25:34 -0700

    Pratik status page (#13)
    * Update GitIgnore
    
    * Create Facility DTO
    
    Every parking garage or lot in known as a facility. We can store the
    facility's capacity and availability here.
    
    * Added New Dependencies
    
    * Created Facility toString Function
    
    * Created CatMap Controller
    
    * Created Facility DAO
    
    * Created Network DAO
    
    * Created Facility Service
    
    * Created Map Page
    
    * Modified DTO to receive data to frontend & inilialized the UI of Catmap.
    
    * Remove Extra White Space
    
    * Remove Extra Lines
    
    ---------
    
    Co-authored-by: Nathan Wick <nathanwick2001@gmail.com>

--- src/main/java/com/catmap/dao/FacilityDAO.java
+++ src/main/java/com/catmap/dao/FacilityDAO.java
@@ -7,7 +7,6 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import com.catmap.dto.FacilityDTO;
 
 @Component
@@ -18,46 +17,39 @@ public class FacilityDAO implements IFacilityDAO {
 	
 	@Override
 	public List<FacilityDTO> fetch() throws Exception {
-		
-		List<FacilityDTO> facilities = new ArrayList<FacilityDTO>();
-		String jsonFacilitiesRaw = networkDAO.request("https://cso.uc.edu:3000/occupancy");
-		JSONArray jsonFacilities = new JSONArray(jsonFacilitiesRaw);
-		
-		for(int i = 0; i < jsonFacilities.length(); i++) {
-			
-			JSONObject jsonFacility = jsonFacilities.getJSONObject(i);
-			FacilityDTO facility = new FacilityDTO();
-			int facilityId = jsonFacility.getInt("FacilityID");
-			String name = jsonFacility.getString("Description");
-			facility.setFacilityId(facilityId);
-			facility.setName(name);
-			
-			JSONArray jsonFacilityOccupancies = jsonFacility.getJSONArray("Occupancy");
-			JSONObject jsonFacilityOccupancy = jsonFacilityOccupancies.getJSONObject(0);
-			int capacity = jsonFacilityOccupancy.getInt("Capacity");
-			int available = jsonFacilityOccupancy.getInt("Available");
-			
-			if(available < 0) {
-				
-				available = 0;
-				
-			}
-			
-			if(available > capacity) {
-				
-				available = capacity;
-				
-			}
-			
-			facility.setCapacity(capacity);
-			facility.setAvailable(available);
-			
-			facilities.add(facility);
-			
-		}
-		
-		return facilities;
-		
+
+	    List<FacilityDTO> facilities = new ArrayList<FacilityDTO>();
+	    String jsonFacilitiesRaw = networkDAO.request("https://cso.uc.edu:3000/occupancy");
+	    JSONArray jsonFacilities = new JSONArray(jsonFacilitiesRaw);
+	    
+	    for(int i = 0; i < jsonFacilities.length(); i++) {
+	        JSONObject jsonFacility = jsonFacilities.getJSONObject(i);
+	        int facilityId = jsonFacility.getInt("FacilityID");
+	        FacilityDTO facility = new FacilityDTO(facilityId);
+	        String name = jsonFacility.getString("Description");
+	        facility.setName(name);
+	        
+	        JSONArray jsonFacilityOccupancies = jsonFacility.getJSONArray("Occupancy");
+	        JSONObject jsonFacilityOccupancy = jsonFacilityOccupancies.getJSONObject(0);
+	        int capacity = jsonFacilityOccupancy.getInt("Capacity");
+	        int available = jsonFacilityOccupancy.getInt("Available");
+	        
+	        if(available < 0) {
+	            available = 0;
+	        }
+	        
+	        if(available > capacity) {
+	            available = capacity;
+	        }
+	        
+	        facility.setCapacity(capacity);
+	        facility.setAvailable(available);
+	        
+	        facilities.add(facility);
+	    }
+	    
+	    return facilities;
 	}
+
 	
 }
