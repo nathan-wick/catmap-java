44dadc8d66db6c823d38c2b459a53972f1c699ef
pratxks <101081557+pratxks@users.noreply.github.com>
2023-04-05 17:40:47 +0530

    Modified DTO to receive data to frontend & inilialized the UI of Catmap.

--- src/main/java/com/catmap/CatmapController.java
+++ src/main/java/com/catmap/CatmapController.java
@@ -6,31 +6,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 import com.catmap.dto.FacilityDTO;
 import com.catmap.service.IFacilityService;
 
 @Controller
 public class CatmapController {
-	
+
 	@Autowired
 	private IFacilityService facilityService;
 
-	@RequestMapping("/")
-	public ModelAndView index(Model model) {
+	@GetMapping("/")
+	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView();
 		List<FacilityDTO> facilities = new ArrayList<FacilityDTO>();
 		try {
 			facilities = facilityService.fetch();
 			modelAndView.setViewName("map");
+			modelAndView.addObject("facilities", facilities);
 		} catch (Exception error) {
 			error.printStackTrace();
 			modelAndView.setViewName("error");
 		}
-		modelAndView.addObject("facilities", facilities);
 		return modelAndView;
 	}
+	
+	
 
 }
+
--- src/main/java/com/catmap/dto/FacilityDTO.java
+++ src/main/java/com/catmap/dto/FacilityDTO.java
@@ -1,6 +1,10 @@
 package com.catmap.dto;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+
 
 public class FacilityDTO {
 	
@@ -9,41 +13,82 @@ public class FacilityDTO {
 	private int capacity;
 	private int available;
 	private Date date;
+	private double lat;
+	private double lng;
 
-	public int getFacilityId() {
-		return facilityId;
+		// Hardcoded latitude and longitude values for each facility ID
+	private static final Map<String, Double[]> FACILITY_LOCATIONS = new HashMap<>() {{
+	    put("2045", new Double[]{39.129894, -84.516852});
+	    put("2046", new Double[]{39.128439, -84.516616});
+	    put("2047", new Double[]{39.135716, -84.515223});
+	    put("2049", new Double[]{39.134303, -84.517271});
+	    put("2043", new Double[]{39.129001, -84.512904});
+	    put("2050", new Double[]{39.137669, -84.505159});
+	    put("2052", new Double[]{39.136808, -84.507685});
+	    put("2051", new Double[]{39.130841, -84.521377});
+	    put("2061", new Double[]{39.1287534,-84.5159829});
+	    put("2042", new Double[]{39.134615, -84.510986});
+	    put("2060", new Double[]{39.134505, -84.510986});
+	    put("2044", new Double[]{39.130166, -84.515964});
+	    put("2048", new Double[]{39.135025, -84.515180});
+	}};
+
+	public FacilityDTO(int facilityId) {
+	    this.facilityId = facilityId;
+
+	    // Set latitude and longitude values based on facility ID
+	    Double[] location = FACILITY_LOCATIONS.get(String.valueOf(facilityId));
+	    if (location != null && location.length == 2) {
+	        this.lat = location[0];
+	        this.lng = location[1];
+	    }
 	}
-	public void setFacilityId(int facilityId) {
-		this.facilityId = facilityId;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getCapacity() {
-		return capacity;
-	}
-	public void setCapacity(int capacity) {
-		this.capacity = capacity;
-	}
-	public int getAvailable() {
-		return available;
-	}
-	public void setAvailable(int available) {
-		this.available = available;
-	}
-	public Date getDate() {
-		return date;
-	}
-	public void setDate(Date date) {
-		this.date = date;
-	}
+
 	
-	@Override
-	public String toString() {
-		return name + ": " + available + " spots available of " + capacity;
-	}
 
-}
+
+			public int getFacilityId() {
+				return facilityId;
+			}
+			public void setFacilityId(int facilityId) {
+				this.facilityId = facilityId;
+			}
+			public String getName() {
+				return name;
+			}
+			public void setName(String name) {
+				this.name = name;
+			}
+			public int getCapacity() {
+				return capacity;
+			}
+			public void setCapacity(int capacity) {
+				this.capacity = capacity;
+			}
+			public int getAvailable() {
+				return available;
+			}
+			public void setAvailable(int available) {
+				this.available = available;
+			}
+			public Date getDate() {
+				return date;
+			}
+			public void setDate(Date date) {
+				this.date = date;
+			}
+			public double getLat() {
+				return lat;
+			}
+			public void setLat(double lat) {
+				this.lat = lat;
+			}
+			public double getLng() {
+				return lng;
+			}
+			public void setLng(double lng) {
+				this.lng = lng;
+			}
+		
+		    
+		}
