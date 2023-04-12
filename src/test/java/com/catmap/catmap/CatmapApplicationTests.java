c3ccc9aa00ffc6186d3eb0a2f2fcbfdb1219be1e
EVAN O'BRIEN <70491578+obrienep@users.noreply.github.com>
2023-04-12 09:52:53 -0400

    Fetch Facility Unit Test (#23)
    * FetchUnitTest WIP
    
    Commiting to unit-test branch while the API is down
    
    * cleaned up imports

--- src/test/java/com/catmap/catmap/CatmapApplicationTests.java
+++ src/test/java/com/catmap/catmap/CatmapApplicationTests.java
@@ -1,13 +1,45 @@
 package com.catmap.catmap;
 
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.catmap.dto.FacilityDTO;
+import com.catmap.service.IFacilityService;
 
 @SpringBootTest
 class CatmapApplicationTests {
 
+	@Autowired
+	IFacilityService facilityService;
+	List<FacilityDTO> facilities;
+	List<FacilityDTO> test = null;
+		
 	@Test
 	void contextLoads() {
+	}
+	//Unit test for fetch function
+	@Test
+	public void facilityFetchValidation() throws Exception {
+		whenFetchIsMade();
+		checkIfWorking();
+	}
+
+
+	//test fetch
+	private void whenFetchIsMade() throws Exception {
+		facilities = facilityService.fetch();
+		
+	}
+	//Asserts fetch() does not return null
+	private void checkIfWorking() {
+		assertNotEquals(facilities, test);
+		
 	}
 
 }
